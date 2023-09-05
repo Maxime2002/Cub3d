df322ee6decf2af950a2af1b9a297ab762ed078e
Kylian Aubry <kyaubry@c2r9p1.42lehavre.fr>
2023-09-05 14:37:51 +0200

    cub3d

--- kylian/cub3d.h
+++ kylian/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: kyaubry <kyaubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 14:02:01 by kyaubry           #+#    #+#             */
-/*   Updated: 2023/09/05 14:07:34 by kyaubry          ###   ########.fr       */
+/*   Updated: 2023/09/05 14:37:24 by kyaubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 /* ==================== error ==================== */
 
 # define ERRCODE_MALLOC 1
-# define ERRMSG_MALLOC "ERROR :\nMalloc failed to allocate memory.\n"
+# define ERRMSG_MALLOC "Error\nMalloc failed to allocate memory.\n"
 
 /* ==================== include ==================== */
 
@@ -28,12 +28,43 @@
 
 /* ==================== structure ==================== */
 
+typedef struct s_ceiling
+{
+	int		r;
+	int		g;
+	int		b;
+
+}			t_ceiling;
+
+typedef struct s_ground
+{
+	int		r;
+	int		g;
+	int		b;
+
+}			t_ground;
+
+typedef struct s_info
+{
+	char	**map;
+	void	*no;
+	void	*so;
+	void	*we;
+	void	*ea;
+
+	struct s_ceiling ceiling;
+	struct s_ground ground;
+
+}			t_info;
+
+s_info ft_parcing(int argc, char **argv);
+
 /* ==================== function error ==================== */
 
-int	print_error_message(int error_code);
+int			print_error_message(int error_code);
 
 /* ==================== function utils ==================== */
 
-int	ft_strlen(char *src);
+int			ft_strlen(char *src);
 
 #endif
